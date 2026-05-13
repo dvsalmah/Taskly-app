405f64492f5c83c6a2ac9f02df92723c747b45b0
dvsalmah <dealovasalmah12@gmail.com>
2026-05-14 00:27:00 +0700

    chore: setup configurations and static assets

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,22 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+    server: {
+        host: '127.0.0.1',
+        port: 5173,
+        hmr: {
+            host: '127.0.0.1',
+        },
+    },
     plugins: [
         laravel({
             input: 'resources/js/app.jsx',
             refresh: true,
         }),
         react(),
+        tailwindcss(),
     ],
 });
