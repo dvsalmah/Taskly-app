ff5a2d43d6a6c8e5ce7bff2db5206d33f3c4856e
dvsalmah <dealovasalmah12@gmail.com>
2026-06-11 21:53:34 +0700

    fix: add SSL fallback

--- config/database.php
+++ config/database.php
@@ -58,9 +58,11 @@ return [
             'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
-            'options' => extension_loaded('pdo_mysql') && env('MYSQL_ATTR_SSL_CA') ? array_filter([
+            'options' => extension_loaded('pdo_mysql') && env('MYSQL_ATTR_SSL_CA') && file_exists(base_path(env('MYSQL_ATTR_SSL_CA'))) ? array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => base_path(env('MYSQL_ATTR_SSL_CA')),
-            ]) : [],
+            ]) : (extension_loaded('pdo_mysql') ? array_filter([
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA') === 'isrgrootx1.pem' ? '/etc/ssl/certs/ca-certificates.crt' : null,
+            ]) : []),
         ],
 
         'pgsql' => [
